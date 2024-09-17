933f04ba5fd37216addc91c3d84037e4dc974d52
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-09-17 10:53:10 -0400

    change in yaml

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,8 +3,8 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		//System.out.println("Not the right string, tests will fail!");
-		//hello
+		System.out.println("Not the right string, tests will fail!");
+		
 	}
 
 }
